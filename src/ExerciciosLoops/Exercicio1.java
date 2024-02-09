7c44e09a8995c47a35cded7b74cf2d05a6fe8940
Dorivania Santos <dorim.dev@gmail.com>
2024-02-09 16:29:41 -0300

    exercicios loops

---
+++ src/ExerciciosLoops/Exercicio1.java
@@ -0,0 +1,35 @@
+package ExerciciosLoops;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		
+		int menorNumero, maiorNumero;
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro número:");
+		menorNumero = leia.nextInt();
+		System.out.println("Digite o segundo número:");
+		maiorNumero = leia.nextInt();
+		
+		if(menorNumero >= maiorNumero) {
+			System.out.println("O primeiro número digitado é maior que o segundo, intervalo inválido!");
+		} else {
+			System.out.println("\nNo Intervalo entre " + menorNumero + " e " + maiorNumero + ":\n");
+		}
+			
+			
+			
+		for (int i = menorNumero; i <= maiorNumero; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println(i + " é múltiplo de 3 e 5.");
+			}
+			
+		}
+		
+		leia.close();
+	}
+
+}
---
+++ src/ExerciciosLoops/Exercicio5.java
@@ -0,0 +1,25 @@
+package ExerciciosLoops;
+
+import java.util.Scanner;
+
+public class Exercicio5 {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+		int numero, soma = 0;
+
+		do {
+			System.out.println("Digite um número (para finalizar, digite 0): ");
+			numero = leia.nextInt();
+			if (numero > 0)
+				soma = numero + soma;
+		} while (numero != 0);
+
+		System.out.println("A soma dos números positivos é: " + soma);
+
+		leia.close();
+	}
+
+}
