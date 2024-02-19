c9a98613e8942347f99ae639e53ec3f63b1c6b0d
Dorivania Santos <dorim.dev@gmail.com>
2024-02-19 20:50:35 -0300

    exercicios Vetores e Matrizes

---
+++ src/ExerciciosVetoresMatrizes/Exercicio1Vetor.java
@@ -0,0 +1,30 @@
+package ExerciciosVetoresMatrizes;
+
+import java.util.Scanner;
+
+public class Exercicio1Vetor {
+
+	public static void main(String[] args) {
+
+		int[] v = { 2, 5, 1, 3, 4, 9, 7, 8, 10, 6 };
+
+		int numero;
+		String msg = "";
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Digite o número que você deseja encontrar: ");
+		numero = sc.nextInt();
+
+		for (int i = 0; i < v.length; i++) {
+			if (v[i] == numero) {
+				msg = "O número " + numero + " está localizado na posição: " + i;
+				break;
+			} else
+				msg = "O número " + numero + " não foi encontrado";
+		}
+
+		System.out.println(msg);
+
+		sc.close();
+	}
+}
---
+++ src/ExerciciosVetoresMatrizes/Exercicio3Matriz.java
@@ -0,0 +1,43 @@
+package ExerciciosVetoresMatrizes;
+
+public class Exercicio3Matriz {
+
+	public static void main(String[] args) {
+		int[][] matriz = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
+		int[] diagonalPrincipal = new int[3];
+		int[] diagonalSecundaria = new int[3];
+
+		int somaDiagonalPrincipal = 0;
+		int somaDiagonalSecundaria = 0;
+
+		for (int i = 0; i < matriz.length; i++) {
+			for (int j = 0; j < matriz.length; j++) {
+				if (i == j) {
+					diagonalPrincipal[i] = matriz[i][j];
+				}
+				if (j == (matriz.length - 1 - i)) {
+					diagonalSecundaria[i] = matriz[i][j];
+				}
+			}
+		}
+
+		System.out.println("Elementos da Diagonal Principal:");
+		for (int x = 0; x < diagonalPrincipal.length; x++) {
+			System.out.print(" " + diagonalPrincipal[x]);
+			somaDiagonalPrincipal += diagonalPrincipal[x];
+		}
+
+		System.out.println("\nElementos da Diagonal Secundária:");
+
+		for (int x = 0; x < diagonalSecundaria.length; x++) {
+			System.out.print(" " + diagonalSecundaria[x]);
+			somaDiagonalSecundaria += diagonalSecundaria[x];
+		}
+
+		System.out.println("\n\nSoma dos Elementos da Diagonal Principal:\n" + somaDiagonalPrincipal);
+		System.out.println("\nSoma dos Elementos da Diagonal Secundária:\n" + somaDiagonalSecundaria);
+
+	}
+
+}
